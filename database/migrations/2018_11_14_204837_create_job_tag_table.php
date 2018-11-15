a1f9b15e2b516af2a39a3cf97efefba191a398fd
xLeDocteurx <lenoirc.fr@gmail.com>
2018-11-15 13:42:24 +0100

    fixed errors in migrations

--- database/migrations/2018_11_14_204837_create_job_tag_table.php
+++ database/migrations/2018_11_14_204837_create_job_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJobtagTable extends Migration
+class CreateJobTagTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_11_14_210010_create_tagfamilies_table.php
+++ database/migrations/2018_11_14_210010_create_tagfamilies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTagfamilyTable extends Migration
+class CreateTagfamiliesTable extends Migration
 {
     /**
      * Run the migrations.
