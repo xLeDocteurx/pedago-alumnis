82336e762c5ea80b6f5513760a0c4c3aa531826a
xLeDocteurx <lenoirc.fr@gmail.com>
2018-11-18 12:20:43 +0100

    add link to access current connected user profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
+                                    <a class="dropdown-item" href="{{ route('users_show', Auth::user()->id) }}">{{ __('Profil') }}</a>
+                                    
                                     <a class="dropdown-item" href="{{ route('contacts') }}">{{ __('Contacts') }}</a>
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
